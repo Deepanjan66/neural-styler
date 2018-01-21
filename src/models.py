10c563a99b4d1ae03bbfbfff53d5513840542506
Deepanjan <deepanjan.chakrabarty@student.unsw.edu.au>
2018-01-21 14:57:34 +1100

    Added concatenation but the upsampling needs to be fixed

--- src/models.py
+++ src/models.py
@@ -1,41 +1,56 @@
 from keras.applications import vgg19
 from keras.optimizers import SGD
 from keras.preprocessing import image
-from keras.layers import Input, Flatten, Dense, Conv2D
+from keras.layers import Input, Flatten, Dense, Conv2D, UpSampling2D
 from keras.models import Model
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.normalization import BatchNormalization
+from keras.layers import concatenate
 import matplotlib.pyplot as plt
 import numpy as np
+
+from utilities import *
 
 
 class NeuralModel:
     def __init__(self, pretrained_model, input_shape):
         self.pretrained_model = pretrained_model
         self.input_shape = input_shape
+        self.network = None
 
-    def define_generator_model(size=256, num_tensors=6):
+    def define_generator_model(self, dim=256, num_tensors=6):
         inputs = []
         for i in range(num_tensors):
-            inputs.append(Input(shape=(size/i, size/i), name="z_input" + str(i)))
+            inputs.append(Input(shape=(dim/(2**i), dim/(2**i), 3), name="z_input" + str(i)))
 
         models = []
         for z_input in inputs:
-            model = get_conv_block(z_input, filters=8, kernel_size=(3,3),\
-                                   normalization=BatchNormalization(), \
-                                   activation=LeakyReLU(alpha=.001))
-
-            model = get_conv_block(model, filters=8, kernel_size=(3,3),\
-                                   normalization=BatchNormalization()), \
-                                   activation=LeakyReLU(alpha=.001))
-
-            model = get_conv_block(model, filters=8, kernel_size=(1,1),\
+            model = get_conv_block(num_blocks=3, model_input=z_input, filters=8, 
+                                   kernels=[(3,3), (3,3), (1,1)],\
                                    normalization=BatchNormalization(), \
                                    activation=LeakyReLU(alpha=.001))
 
             models.append(model)
+        
+        for i in reversed(range(len(models))):
+
+            upsampled_output = UpSampling2D(size=(2,2), data_format=None)(models[i])
+            prev_normalized = BatchNormalization()(upsampled_output)
+            next_normalized = BatchNormalization()(models[i-1])
+            print(prev_normalized, "And another: ", next_normalized)
+            exit(1)
+
+            merged_model = concatenate([prev_scale, model])
 
 
+        self.network = model
+        
+        return models
+
+
+
+network = NeuralModel(None, (0,))
+network.define_generator_model()
 """
 img = np.array(image.load_img('test.png', target_size=(3, 224, 224)))
 img = np.expand_dims(img, axis=0)
--- src/utilities.py
+++ src/utilities.py
@@ -1,7 +1,7 @@
 from keras.layers import Conv2D
 
-def get_conv_block(model_input, filters, \
-                   kernel, activation, \
+def get_conv_block(num_blocks, model_input, filters, \
+                   kernels, activation, \
                    normalization):
     """
     :param model_input: Input vector for the model
@@ -10,10 +10,24 @@ def get_conv_block(model_input, filters, \
     :param activation: Activation function to be used in this layer
     :para, normalization: Normalization to be used in this layer
     """
-    model = Conv2D(filters=filters, kernel_size=kernel_size, \
-            strides=(1,1), padding="valid", \
-            activation="linear")(model_input)
-    model = normalization(model)
-    model = activation(model)
+    list_args = convert_to_list(num_blocks, filters=filters, \
+                                kernels=kernels, \
+                                activation=activation, \
+                                normalization=normalization)
+    model = model_input
+    for i in range(num_blocks):
+        model = Conv2D(filters=list_args['filters'][i], kernel_size=list_args['kernels'][i], \
+                strides=(1,1), padding="valid", \
+                activation="linear", data_format="channels_last")(model)
+        model = list_args['normalization'][i](model)
+        model = list_args['activation'][i](model)
 
     return model
+
+def convert_to_list(num_blocks, **args):
+    for arg, val in args.items():
+        if type(val) is not list:
+            args[arg] = [val for _ in range(num_blocks)]
+
+    return args
+
