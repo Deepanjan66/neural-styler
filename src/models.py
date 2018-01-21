98610620e0d5181961d8aedb28eb0e1be0efc5fd
Deepanjan <deepanjan.chakrabarty@student.unsw.edu.au>
2018-01-21 20:32:37 +1100

    Added pretrained model to class for network

--- src/models.py
+++ src/models.py
@@ -6,6 +6,7 @@ from keras.models import Model
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.normalization import BatchNormalization
 from keras.layers import concatenate
+from keras import backend as K
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -14,14 +15,23 @@ from utilities import *
 
 class NeuralModel:
     def __init__(self, pretrained_model, input_shape):
-        self.pretrained_model = pretrained_model
         self.input_shape = input_shape
         self.network = None
+        if pretrained_model:
+            self.add_pretrained_model(pretrained_model)
 
-    def define_generator_model(self, dim=256, num_tensors=6):
+    def add_pretrained_model(self, model):
+        self.pretrained_model = pretrained_model
+        outputs = [layer.output for i, layer in \
+                   enumerate(self.pretrained_model.layers)\
+                   if i in [1,4,7,12,17]]
+        self.pred_functors = [K.function([inp]+ [K.learning_phase()], [out])\
+                                                for out in outputs]
+
+    def define_generator_model(self, dim=256, num_tensors=6, num_channels=3):
         inputs = []
         for i in range(num_tensors):
-            inputs.append(Input(shape=(dim/(2**i), dim/(2**i), 3), name="z_input" + str(i)))
+            inputs.append(Input(shape=(dim/(2**i), dim/(2**i), num_channels), name="z_input" + str(i)))
 
         models = []
         for z_input in inputs:
@@ -45,7 +55,8 @@ class NeuralModel:
             next_normalized = BatchNormalization()(models[i-1])
             merged_models = concatenate([prev_normalized, next_normalized])
 
-            merged_models = get_conv_block(num_blocks=3, model_input=merged_models, filters=curr_filters, 
+            merged_models = get_conv_block(num_blocks=3, model_input=merged_models, \
+                                   filters=curr_filters, \
                                    kernels=[(3,3), (3,3), (1,1)],\
                                    normalization=BatchNormalization(), \
                                    activation=LeakyReLU(alpha=.001))
@@ -58,7 +69,7 @@ class NeuralModel:
                                activation=LeakyReLU(alpha=.001))
 
         texture_image = get_conv_block(num_blocks=1, model_input=final_out, \
-                               filters=3,\
+                               filters=num_channels,\
                                kernels=[(1,1)],\
                                normalization=BatchNormalization(), \
                                activation=LeakyReLU(alpha=.001))
@@ -67,18 +78,34 @@ class NeuralModel:
         self.model.compile(optimizer='sgd', loss='mean_squared_error')
     
     def fit_through_pretrained_network(self, **kwargs):
+        if not self.pred_functors:
+            raise ValueError("Please provide pretrained model for training")
+
         content_images = kwargs['content_images']
+        targets = {}
+        targets['content_images'] = []
         for img in content_images:
             self.pretrained_model.predict(img)
 
 
-    def fit(self, inputs):
+    def fit(self, image):
+        inp = self.pretrained_model.input
+        outputs = [layer.output for i, layer in enumerate(self.pretrained_model.layers)\
+                                                if i in [1,4,7,12,17]]
+        functors = [K.function([inp]+ [K.learning_phase()], [out]) for out in outputs]
+        layer_outs = [func([img, 1.]) for func in functors]
+        print(layer_outs)
         
 
 
 
 network = NeuralModel(vgg19.VGG19(include_top=False, weights='imagenet'), (0,))
 network.define_generator_model()
+
+img = np.array(image.load_img('test.png', target_size=(224, 224, 3)))
+img = np.expand_dims(img, axis=0)
+model = vgg19.VGG19(include_top=False, weights='imagenet')
+network.fit(img)
 """
 img = np.array(image.load_img('test.png', target_size=(3, 224, 224)))
 img = np.expand_dims(img, axis=0)
