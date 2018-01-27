bad1477674ef275185d5aa3c58de43baf4caae34
Deepanjan <deepanjan.chakrabarty@student.unsw.edu.au>
2018-01-28 01:58:03 +1100

    Added tensorflow session for converting tensor to np array

--- src/loss_func.py
+++ src/loss_func.py
@@ -1,3 +1,4 @@
+from keras import backend as K
 import numpy as np
 
 from configs import pretrained_network
@@ -5,10 +6,11 @@ from utilities import get_model_layers
 
 def mean_squared_loss(y_true, y_pred):
     pred_functors = get_model_layers(pretrained_network)
-    targets = []
-    print(y_true)
+    sess = K.get_session()
+    print(y_pred.eval(session=sess))
+
+    #targets = []
+    #targets.append(np.array([np.array(func([y_pred, 1.]))[0][0] for func in pred_functors['style']]))
+    #print(targets.shape)
     exit(1)
-    y_true = np.array(y_true[0])
-    targets.append([np.array(func([y_true, 1.]))[0][0] for func in pred_functors['style']])
-    print(targets)
     return y_pred
